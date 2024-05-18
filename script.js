849eda54a0669408038ce9264a05aa23dc24e272
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-05-18 23:36:47 +0200

    init

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    // console.log('ok')
+
+
+    let grid = document.getElementById('grid')
+    // console.log(grid)
+
+    let largeur = 100   ;
+    let total = largeur * largeur
+
+
+    for(let i = 0; i < total; i++){
+
+        let square = document.createElement('div')
+        grid.appendChild(square)
+        square.classList.add('square')
+
+        square.noir = false
+
+
+        square.addEventListener('mouseover', ()=>{
+            if(square.noir){
+                square.style.backgroundColor = 'white'
+            }else{
+                square.style.backgroundColor = 'black'
+            }
+            square.noir = !square.noir
+        })
+
+
+    }
+
+
+
+})
